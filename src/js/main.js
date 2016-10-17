26c9877091624179cb4dcf15f06f0af6a6cad7bd
emma <emmaruthoneill@gmail.com>
2016-10-17 15:06:04 -0700

    added in incomplete results for governor/house/senate

--- src/js/main.js
+++ src/js/main.js
@@ -7,10 +7,11 @@ var blue = "#265B9B";//"#194E8E";//"#315A8C";//"#004366";//"#62A9CC";
 var light_blue = "#598ECE";
 var green = "#487F75";//"#2E655B";
 var purple = "#69586B";
+var orange = "#DE8067";
 var yellow = "#FFCC32";//"#6790B7";//"#EB8F6A";//"#FFFF65";//"#FFCC32";
-var yes_blue = '#61988E';//"#705A91";//"#1D75AF";//"#6C85A5";//"#FFE599";
-var no_red = '#EB8F6A';//"#FFDB89";//"#EAE667";//"#D13D59";//"#6790B7";
-var undecided_yellow = "#b2b2b2";//"#EB8F6A";//"#FFFF65";
+var yes_map = '#61988E';//"#705A91";//"#1D75AF";//"#6C85A5";//"#FFE599";
+var no_map = '#EB8F6A';//"#FFDB89";//"#EAE667";//"#D13D59";//"#6790B7";
+var undecided_map = "#b2b2b2";//"#EB8F6A";//"#FFFF65";
 var dark_gray = "#8C8C8C";
 var light_gray = "#b2b2b2";
 
@@ -126,8 +127,14 @@ function color_partial_results(tempvar,properties){
         } else if (tempvar["c"+count+"_party"] == "GOP") {
           // return red;
           return "url(#hashred)";
-        } else {
+        } else if (tempvar["c"+count+"_name"] == "Jill Stein"){
+          console.log("Jill!!!");
           return green;
+        } else if (tempvar["c"+count+"_name"] == "Gary Johnson"){
+          console.log("Gary!!!");
+          return orange;
+        } else {
+          return yellow;
         }
       }
       count++;
@@ -163,8 +170,14 @@ function code_county(tempvar,properties){
           }
         } else if (tempvar["c"+count+"_party"] == "GOP") {
           return red;
-        } else {
+        } else if (tempvar["c"+count+"_name"] == "Jill Stein"){
+          console.log("Jill!!!");
           return green;
+        } else if (tempvar["c"+count+"_name"] == "Gary Johnson"){
+          console.log("Gary!!!");
+          return orange;
+        } else {
+          return yellow;
         }
       }
       count++;
@@ -176,11 +189,11 @@ function code_county(tempvar,properties){
 function code_map_variable(tempvar,properties){
   if (tempvar.r) {
     if (tempvar.r["Yes"] > tempvar.r["No"]) {
-      return yes_blue;
+      return yes_map;
     } else if (tempvar.r["Yes"] < tempvar.r["No"]){
-      return no_red;
+      return no_map;
     } else {
-      return undecided_yellow;
+      return undecided_map;
     }
   }
   var count = 1;
@@ -190,8 +203,14 @@ function code_map_variable(tempvar,properties){
         return blue;
       } else if (tempvar["c"+count+"_party"] == "GOP") {
         return red;
-      } else {
+      } else if (tempvar["c"+count+"_name"] == "Jill Stein"){
+        console.log("Jill!!!");
         return green;
+      } else if (tempvar["c"+count+"_name"] == "Gary Johnson"){
+        console.log("Gary!!!");
+        return orange;
+      } else {
+        return yellow;
       }
     }
     count++;
@@ -379,15 +398,12 @@ document.querySelector('#presidentbycounty').addEventListener('click', function(
         if (ind == 0) {
           var stateabbrev = stateCodes[parseInt(d.id)].state;
           if (presidentialData[String(stateabbrev)].d) {
-            console.log(presidentialData[String(stateabbrev)].d);
             var tempvar = presidentialData[String(stateabbrev)];
             var new_color = code_map_variable(tempvar,d.properties);
-            console.log(new_color);
             return new_color;
           } else if (presidentialData[String(stateabbrev)]){
             var tempvar = presidentialData[String(stateabbrev)];
             var new_color = color_partial_results(tempvar,d.properties);
-            console.log(new_color);
             return new_color;
           } else {
             return dark_gray;
@@ -549,7 +565,8 @@ document.querySelector('#congressmap').addEventListener('click', function(){
                 var new_color = code_map_variable(tempvar,d.properties);
                 return new_color;
               } else {
-                return dark_gray;
+                var new_color = color_partial_results(tempvar,d.properties);
+                return new_color;
               }
           } else {
             return light_gray;//fill(path.area(d));
@@ -562,7 +579,8 @@ document.querySelector('#congressmap').addEventListener('click', function(){
                 var new_color = code_map_variable(tempvar,d.properties);
                 return new_color;
               } else {
-                return dark_gray;
+                var new_color = color_partial_results(tempvar,d.properties);
+                return new_color;
               }
           } else {
             return light_gray;//fill(path.area(d));
@@ -575,7 +593,8 @@ document.querySelector('#congressmap').addEventListener('click', function(){
                 var new_color = code_map_variable(tempvar,d.properties);
                 return new_color;
               } else {
-                return dark_gray;
+                var new_color = color_partial_results(tempvar,d.properties);
+                return new_color;
               }
           } else {
             return light_gray;//fill(path.area(d));
